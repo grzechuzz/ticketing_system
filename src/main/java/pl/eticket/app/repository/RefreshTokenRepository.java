6e6622193ed85ecbc1389bdc660f78e61330e211
grzechuzz <grzechu.rz@gmail.com>
2026-01-14 16:35:46 +0100

    Remove useless @Repository annotation

--- src/main/java/pl/eticket/app/repository/RefreshTokenRepository.java
+++ src/main/java/pl/eticket/app/repository/RefreshTokenRepository.java
@@ -3,13 +3,11 @@ package pl.eticket.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import pl.eticket.app.entity.RefreshToken;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     @Query("SELECT rt FROM RefreshToken rt WHERE rt.tokenHash = :hash AND rt.revokedAt IS NULL AND rt.expiresAt > :now")
     Optional<RefreshToken> findValidByTokenHash(String hash, Instant now);
