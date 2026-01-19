6190bbf8d35376d350a1ffacd561e73930284354
grzechuzz <grzechu.rz@gmail.com>
2026-01-19 15:31:27 +0100

    Refactor OrderItemResponse DTO, add new fields

--- src/main/java/pl/eticket/app/dto/order/OrderItemResponse.java
+++ src/main/java/pl/eticket/app/dto/order/OrderItemResponse.java
@@ -1,9 +1,13 @@
 package pl.eticket.app.dto.order;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 public record OrderItemResponse(
     Long id,
+    Long eventId,
+    String eventName,
+    Instant eventDate,
     String sectorName,
     String ticketTypeName,
     Integer row,
